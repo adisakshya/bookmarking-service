af61f73f843bc7d696992cf185c72c962d4f186d
adisakshya <adisakshya98@gmail.com>
2020-05-07 15:51:09 +0530

    added docs for ping controller

--- src/bookmark/controllers/ping/controller.js
+++ src/bookmark/controllers/ping/controller.js
@@ -8,48 +8,75 @@
 const dbController = require('../../lib/checkDBConnection');
 
 /**
- * Handle Ping request
- * @param {object} req 
- * @param {object} res 
+ * @api {get} /api/v1/ping ping the server
+ * @apiVersion 1.0.0
+ * @apiName ping the server
+ * @apiGroup ping
+ * @apiPermission all
+ * 
+ * @apiParamExample {String} request-example
+ * 
+ * curl --request GET http://<domain:port>/api/v1/ping
+ * 
+ * @apiParamExample {json} response-example
+ * 
+ * {
+ *      "message": "Pong",
+ * }
  */
 const _ping = async (req, res) => {
 
     return res
-      .status(200)
-      .json({
-          "message": "Pong"
-      });
+        .status(200)
+        .json({
+            "message": "Pong"
+        });
 
 }
 
 /**
- * Check Connection with Database
- * @param {object} req 
- * @param {object} res 
+ * @api {get} /api/v1/ping/db check connection with database
+ * @apiVersion 1.0.0
+ * @apiName check connection with database
+ * @apiGroup ping
+ * @apiPermission admin
+ * 
+ * @apiParamExample {String} request-example
+ * 
+ * curl --request GET http://<domain:port>/api/v1/ping/db
+ * 
+ * @apiParamExample {json} response-example
+ * 
+ * {
+ *      "success": true,
+ *      "error": false,
+ *      "message": "Connection with database is successfully established",
+ *      "data": null
+ * }
  */
 const _checkDBConnection = async (req, res) => {
 
     // CHECK if connection is established
     if(await dbController.checkDB() !== 1) {
-      // if not then return Status 504
-      return res
-        .status(504)
-        .json({
-            "success": false,
-            "error": true,
-            "message": "Failed to establish connection with database",
-            "data": null
-        });
+        // if not then return Status 504
+        return res
+            .status(504)
+            .json({
+                "success": false,
+                "error": true,
+                "message": "Failed to establish connection with database",
+                "data": null
+            });
     } else {
-      // if yes then return Status 200 OK
-      return res
-        .status(200)
-        .json({
-            "success": true,
-            "error": false,
-            "message": "Connection with database is successfully established",
-            "data": null
-        });
+        // if yes then return Status 200 OK
+        return res
+            .status(200)
+            .json({
+                "success": true,
+                "error": false,
+                "message": "Connection with database is successfully established",
+                "data": null
+            });
     }
 }
 
